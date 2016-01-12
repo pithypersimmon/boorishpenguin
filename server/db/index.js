d87571c590d5dd102e4e8e0a7cd87b376f04e67f
Joshua Huang <jphuangjr@gmail.com>
2016-01-12 11:55:50 -0800

    Modified db information

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,9 @@
 var Sequelize = require('sequelize');
 
-var database = process.env.DATABASE || 'jmuspkeyvjzsvvwp';
-var dbUser = process.env.DBUSER || 'htmaaabw4pe3k9ja';
-var dbPass = process.env.DBPASS;
-var dbHost = process.env.DBHOST || 'jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+var database = process.env.DATABASE || 'pithypersimmons';
+var dbUser = process.env.DBUSER || 'root';
+var dbPass = process.env.DBPASS || "student";
+var dbHost = process.env.DBHOST || 'localhost'
 
 var db = new Sequelize(database, dbUser, dbPass, {
   host: dbHost
