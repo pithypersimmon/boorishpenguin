868d13de6561f3b5e441aa2aafc6527dc58a2e75
dangersong <dangersong1@gmail.com>
2016-01-12 13:49:48 -0800

    dev callback and api key update

--- server/config/apikeys.js
+++ server/config/apikeys.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleOauth: {
-    clientID: "456598125555-0nbiekk5rdp1lup82j6hmirp47inhkq1.apps.googleusercontent.com",
-    clientSecret: "IFl-oxG-LGoazw9tmJiavmjl"
+    clientID: "454460627075-bfqu9b986h2eapfcpemfvqes1utouuim.apps.googleusercontent.com",
+    clientSecret: "JlEylvVpDbFsb_gr6M7j6-Rz"
   }
 }
