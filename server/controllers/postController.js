1554438f9a3e1462d5ddee720122dee55ab79c60
Joey Leung <joeyleung8@gmail.com>
2016-01-13 14:54:54 -0800

    Added new/delete/add/read Post and getallquestions

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -8,29 +8,24 @@ module.exports = {
     // contains both products and responses
     db.Post.findAll({
       where: {
-        isAnAnswer: false
+        isAResponse: false
       },
       //This creates the rows that are references instead of 
       //just the numbers
-      include: [db.User, db.Course, db.Tag]
+      include: [db.User]
     })
     //Format the array of questions into objects that our
     //front end will use
     .then(function(products) {
-      var formattedQs = products.map(function(product) {
+      var formattedProducts = products.map(function(product) {
         return {
           id: product.id,
           title: product.title,
           text: product.text,
-          isAnAnswer: false,
+          isAResponse: false,
           points: product.points,
           responses: product.responses,
-          // isAnswered: product.isAnswered,
-          // isGood: product.isGood,
-          // isClosed: product.isClosed,
           createdAt: product.createdAt,
-          // coursename: product.Course.name,
-          // tagname: product.Tag.name,
           user: product.User.name,
           imgUrl: product.User.picture,
           updatedAt: product.updatedAt
@@ -38,31 +33,27 @@ module.exports = {
       });
 
       products = {};
-      products.results = formattedQs;
+      products.results = formattedProducts;
       res.json(products);
     });
   },
 
-  readQuestion: function(req, res) {
+  readPost: function(req, res) {
     var pid = req.params.id;
 
     db.Post.findById(pid, {
-      include: [db.User, db.Course, db.Tag]
+      include: [db.User]
     })
     .then(function(question) {
-      var formattedQ = [{
+      var formattedProduct = [{
         id: question.id,
         title: question.title,
         text: question.text,
-        isAnAnswer: false,
+        isAResponse: false,
         points: question.points,
+        //Number of responses
         responses: question.responses,
-        isAnswered: question.isAnswered,
-        isGood: question.isGood,
-        isClosed: question.isClosed,
         createdAt: question.createdAt,
-        coursename: question.Course.name,
-        tagname: question.Tag.name,
         user: question.User.name,
         userid: question.User.id,
         imgUrl: question.User.picture,
@@ -75,25 +66,24 @@ module.exports = {
         },
         include: [db.User]
       })
-      .then(function(answers) {
-        var formattedAs = answers.map(function(answer) {
+      .then(function(responses) {
+        var formattedResponses = responses.map(function(response) {
           return {
-            id: answer.id,
-            text: answer.text,
-            isAnAnswer: true,
-            points: answer.points,
-            isGood: answer.isGood,
-            QuestionId: qid,
-            user: answer.User.name,
-            userid: answer.User.id,
-            createdAt: answer.createdAt,
-            imgUrl: answer.User.picture
+            id: response.id,
+            text: response.text,
+            isAResponse: true,
+            points: response.points,
+            PostId: pid,
+            user: response.User.name,
+            userid: response.User.id,
+            createdAt: response.createdAt,
+            imgUrl: response.User.picture
           };
         });
 
-        qAndAs = {};
-        qAndAs.results = formattedQ.concat(formattedAs);
-        res.json(qAndAs);
+        productAndResponses = {};
+        productAndResponses.results = formattedProduct.concat(formattedResponses);
+        res.json(productAndResponses);
       });
     });
   },
@@ -107,15 +97,16 @@ module.exports = {
     //Post Only
     var link = req.body.link;
     //Response Only
-    var qid = req.body.id_question;
+    //Check to see when we get these and how we get them
+    var pid = req.body.id_question;
     //How are we given type?
     db.Post.create({
       title: title,
       text: text,
       link: link,
       UserId: uid,
-      QuestionId: qid || null,
-      Type: (qid) ? 'Response' : 'Post' 
+      PostId: pid || null,
+      Type: (pid) ? 'Response' : 'Post' 
 
     })
     .then(function(post) {
@@ -130,23 +121,33 @@ module.exports = {
     var reqName = req.user.profile.emails[0].value;
 
     //This 
-    db.Post.findById(pid)
-    .then(function(post) {
-      post.destroy()
-      .then(function() {
-        res.sendStatus(204);
-      });
-    });
 
     // db.Post.findById(pid)
     // .then(function(post) {
-    //   return post.destroy();
-    // })
-    // .then(function(nothing) {
-    //   res.sendStatus(204);
+    //   post.destroy()
+    //   .then(function(id) {
+    //     // if id
+    //     res.sendStatus(204);
+    //   });
     // });
+
+
+    db.Post.findById(pid)
+    .then(function(post) {
+      return post.destroy();
+    })
+    .then(function(id) {
+      if (id) {
+        res.sendStatus(204);
+      }
+    });
+  },
+  likePost: function(req, res) {
+    //Not done refactoring like yet
+
   }
 };
+
 
 
 
